39f80ae5e91e810ebcc3da536761f48b74e00d1f
Leon <leonsarko7@gmail.com>
2023-08-23 09:30:09 +0200

    Added random choice generator code

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ const choices =  document.querySelector(".choices");
 textArea.focus();
 textArea.addEventListener("keyup", e =>{
     createTags(e.target.value);
+
+    if (e.key === "Enter"){
+        setTimeout(() => {
+            e.target.value = "";
+        }, 10)
+
+
+        randomSelect();
+    }
 } )
 
 function createTags(input){
-    const tags = input.split(",").filter(tag => tag.trim() !== '').map(tag => tag.trim());
+    const tags = input.split(",").filter(tag => tag.trim() !== "").map(tag => tag.trim());
 
     choices.innerHTML = "";
     tags.forEach(tag => {
@@ -21,3 +30,42 @@ function createTags(input){
     
 }
 
+function randomSelect(){
+    const times = 25;
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+        highlightTag(randomTag);
+
+        setTimeout(() =>{
+            removeHighlight(randomTag);
+        }, 100)
+
+
+    }, 100)
+
+        setTimeout(() => {
+            clearInterval(interval);
+
+            setTimeout(() => {
+                const randomChoice = pickRandomTag()
+                highlightTag(randomChoice);
+            }, 100)
+        }, times * 100)
+
+
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll(".choice");
+    return tags[Math.floor(Math.random() * tags.length)];
+
+}
+
+
+function highlightTag(tag){
+    tag.classList.add("selected");
+}
+
+function removeHighlight(tag){
+    tag.classList.remove("selected");
+}
